1b1ff4a873a8b8aad4324130a1d7bbe544e2d9a3
Felix S. Klock II <pnkfelix@pnkfx.org>
2022-02-02 21:30:01 -0500

    checkpoint status circa 2021-11-18.

--- combinatorics/src/main.rs
+++ combinatorics/src/main.rs
@@ -1,16 +1,24 @@
 #![feature(generators)]
 
-use std::collections::HashMap;
-
 use iterator_item::iterator_item;
 
+macro_rules! time_it {
+    ($context:expr, $e:expr) => {
+        {
+            let timer = std::time::Instant::now();
+            let e = $e;
+            println!("{}: {:?}", $context, timer.elapsed());
+            e
+        }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
-    let mut seen = HashMap::new();
-    for (j, cand) in latin_squares(3).enumerate() {
-        let mut entries = seen.entry(cand.clone()).or_insert(Vec::new());
-        entries.push(j);
-        dbg!((j, cand, entries));
+    for dim in 1..=5 {
+        let count = time_it!(format!("LS({})", dim),
+                             latin_squares(dim, true, true).count());
+        dbg!((dim, count));
     }
 }
 
@@ -81,11 +89,16 @@ impl MatrixCxn {
     }
 
     fn to_cards(&self, cand: &[(u16, u16)]) -> Vec<String> {
+        let rank = ['A', 'K', 'Q', 'J'];
+        let suit = ['s', 'h', 'd', 'c'];
         let mut iter = cand.iter()
             .map(|(f,s)| {
                 format!("{}{}",
-                        char::from_u32('a' as u32 + *f as u32).unwrap(),
-                        char::from_u32('α' as u32 + *s as u32).unwrap())
+                        rank[*f as usize],
+                        suit[*s as usize])
+                        // char::from_u32('a' as u32 + *f as u32).unwrap(),
+                        // char::from_u32('α' as u32 + *s as u32).unwrap())
+                        // char::from_u32('0' as u32 + *s as u32).unwrap())
             });
         let mut rows = Vec::with_capacity(self.dim as usize);
         for _ in 0..self.dim {
@@ -100,34 +113,72 @@ impl MatrixCxn {
 }
 
 iterator_item! { *
-    fn* latin_squares(dim: u16) yields Vec<String> {
+    fn* latin_squares(dim: u16, fixed_top: bool, fixed_left: bool) yields Vec<String> {
         dbg!(dim);
         assert!(dim <= 16); // assume for now we can fit one card in a u32
         let m = MatrixCxn { dim };
-        let mut cand: Vec<(u16, u16)> = std::iter::repeat((0, 0)).take((dim * dim) as usize).collect();
 
-        loop {
+        let mut cand: Vec<(u16, u16)> = std::iter::repeat((0,0)).take((dim * dim) as usize).collect();
+
+        let start;
+        if fixed_top {
+            // w.l.o.g. we can assume the first row is (j,j). (You can transform it
+            // to the other solutions via renamings.)
+            for j in 0..dim { cand[j as usize] = (j,j); }
+            start = dim as usize;
+        } else {
+            start = 0;
+        }
+
+        let mut i;
+
+        // every time we continue in this loop, cand[i] has just been
+        // incremented.
+        'cand_i_incr: loop {
             if m.check_cand(&cand[..]) {
-                yield m.to_cards(&cand);
+
+                let mut left_col_sorted = true;
+                for r in 0..dim {
+                    if cand[m.index(r, 0)].0 != r {
+                        left_col_sorted = false;
+                    }
+                }
+                if !fixed_left || left_col_sorted {
+                    yield m.to_cards(&cand);
+                }
             }
-            // increment
-            let mut i = cand.len() - 1;
-            loop {
+
+            if start == cand.len() { return; }
+
+            assert!(cand.len() > 0);
+            i = cand.len() - 1;
+            'try_incr: loop {
+
                 if let Some(c2) = m.next(cand[i]) {
                     cand[i] = c2;
+
+                    // if this candidate doesn't work locally,
+                    // there's no reason to waste time searching
+                    // through all the deeper nodes.
                     if !m.check_cand_at(&cand[..], i) {
-                        // if we cannot locally pass with this, then just keep searching.
-                        continue;
+                        continue 'try_incr;
                     }
+
                     for j in i+1..cand.len() {
-                        cand[j] = (0, 0);
+                        cand[j] = (0,0);
                     }
-                    break;
-                } else if i == 0 {
-                    return;
-                } else {
-                    i -= 1;
+                    continue 'cand_i_incr;
                 }
+
+                // if `m.next` returns None, then we've exhausted the search
+                // space for this item. If its not the root, then reset it to
+                // zero and move one step backwards (effectively backtracking in
+                // graph)
+                if i == start {
+                    return;
+                }
+
+                i -= 1;
             }
         }
     }
